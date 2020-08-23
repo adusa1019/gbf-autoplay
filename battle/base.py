f2e1ddc6a67f63a2abe631ad2b14f8b484a70147
adusa1019 <symphony20030829@yahoo.co.jp>
2020-08-23 09:38:06 +0900

    メカニック用スキル回しを追加

--- battle/base.py
+++ battle/base.py
@@ -123,6 +123,31 @@ class Battle:
         skills[3].click()
         self.driver.find_element_by_class_name("btn-command-back").click()
 
+    def mechanic(self):
+        skills = None
+        while not skills:
+            try:
+                self.utils.wait_and_click_element_by_xpath(
+                    '//div[@class="lis-character0 btn-command-character"]', time=30)
+                skills = self.driver.find_elements_by_class_name("lis-ability")
+            except ElementClickInterceptedException:
+                pass
+            except Exception:
+                traceback.print_exc()
+                breakpoint()
+        # TODO: 時々発生する例外に対処する
+        for i in range(4):
+            for _ in range(10):
+                try:
+                    wait(self.driver,
+                         10).until(ec.element_to_be_clickable((By.CLASS_NAME, "lis-ability")))
+                    skills[i].click()
+                except Exception:
+                    print(f"in click {i}")
+                else:
+                    break
+        self.driver.find_element_by_class_name("btn-command-back").click()
+
     # TODO: ElementNotInteractable に対応する
     def summon(self, index=-1, name=None, loop_count=10):
         self.driver.find_element_by_class_name("summon-on").click()
