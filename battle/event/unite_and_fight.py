e62c30718c53c99a3b6fa4b915622df024426bf8
adusa1019 <symphony20030829@yahoo.co.jp>
2020-08-29 22:38:30 +0900

    2020/08古戦場で実装した

--- battle/event/unite_and_fight.py
+++ battle/event/unite_and_fight.py
@@ -15,13 +15,39 @@ class UniteAndFight(Event):
         self.driver.find_elements_by_class_name("img-btn-raid")[1].click()
         self.driver.find_elements_by_class_name("lis-multi")[2].click()
 
-    def hell_1st_stage(self):
+    def hell_1st_stage_90(self):
         self.driver.find_elements_by_class_name("img-btn-raid")[0].click()
+
+    def hell_2nd_stage_90(self):
+        self.driver.find_elements_by_class_name("img-btn-raid")[0].click()
+        self.driver.find_elements_by_class_name("btn-multi-battle")[0].click()
+
+    def hell_2nd_stage_95(self):
+        self.driver.find_elements_by_class_name("img-btn-raid")[0].click()
+        self.driver.find_elements_by_class_name("btn-multi-battle")[1].click()
+
+    def hell_3rd_stage_90(self):
+        self.driver.find_elements_by_class_name("img-btn-raid")[0].click()
+        self.driver.find_elements_by_class_name("btn-multi-battle")[0].click()
+
+    def hell_3rd_stage_95(self):
+        self.driver.find_elements_by_class_name("img-btn-raid")[0].click()
+        self.driver.find_elements_by_class_name("btn-multi-battle")[1].click()
+
+    def hell_3rd_stage_100(self):
+        self.driver.find_elements_by_class_name("img-btn-raid")[0].click()
+        self.driver.find_elements_by_class_name("btn-multi-battle")[2].click()
 
     targets = {
         "vh": vh,
         "ex": ex,
         "exp": exp,
+        "hell_1st_90": hell_1st_stage_90,
+        "hell_2nd_90": hell_2nd_stage_90,
+        "hell_2nd_95": hell_2nd_stage_95,
+        "hell_3rd_90": hell_3rd_stage_90,
+        "hell_3rd_95": hell_3rd_stage_95,
+        "hell_3rd_100": hell_3rd_stage_100
     }
 
     def select_difficulty(self):
@@ -30,10 +56,18 @@ class UniteAndFight(Event):
             self.use_elixir_half()
 
     def pre_loop_actions(self):
+        if "hell" in self.target:
+            self.battle_routine = self._battle_routine2
+        else:
+            self.battle_routine = self._battle_routine1
         pass
 
-    def battle_routine(self):
+    def _battle_routine1(self):
         self.mechanic()
         self.summon(name="ゼノ・ウォフマナフ")
         self.attack()
+        self.auto_battle()
+
+    def _battle_routine2(self):
+        self.summon(name="ゼノ・ウォフマナフ")
         self.auto_battle()
