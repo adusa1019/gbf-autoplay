4328273c4f64169e3ca1e91c65f442b56cd0519c
adusa <help.me.adusa@gmail.com>
2021-02-09 00:58:26 +0000

    separate battle components and event components

--- battle/event/__init__.py
+++
@@ -1,5 +0,0 @@
-from battle.event.proving_grounds import ProvingGrounds  # NOQA
-from battle.event.showdowns import Showdowns  # NOQA
-from battle.event.story_event import StoryEvent  # NOQA
-from battle.event.unite_and_fight import UniteAndFight  # NOQA
-from battle.event.base import Battle  # NOQA
