72964d2c2b0b2d8912e94806d28adf559c92fe11
adusa <help.me.adusa@gmail.com>
2021-02-18 10:20:10 +0000

    impl buy medal/refactor

--- non_battle/casino/cage.py
+++ non_battle/casino/cage.py
@@ -9,17 +9,20 @@ class Cage:
     def __init__(self, driver):
         self.driver = driver
 
-    def go(self):
-        url = "http://gbf.game.mbga.jp/#casino/exchange"
+    def go(self, is_exchange=True):
+        url = "http://gbf.game.mbga.jp/#casino"
+        if is_exchange:
+            url += "/exchange"
         while self.driver.current_url != url:
             self.driver.get(url)
 
-    def run(self, start=7):
-        self.go()
-        while self.driver.find_elements_by_class_name("prt-page-number")[1].get_attribute(
+    def move_page(self, index):
+        while self.driver.find_elements_by_class_name("prt-page-number")[index].get_attribute(
                 "disable") == "false":
-            self.driver.find_elements_by_class_name("prt-page-number")[1].click()
+            self.driver.find_elements_by_class_name("prt-page-number")[index].click()
             time.sleep(1)
+
+    def daily(self, start):
         while len(self.driver.find_elements_by_class_name("btn-exchange")) > start:
             self.driver.find_elements_by_class_name("btn-exchange")[-1].click()
             time.sleep(1)
@@ -31,3 +34,24 @@ class Cage:
             time.sleep(1)
             self.driver.find_element_by_class_name("btn-usual-ok").click()
             time.sleep(1)
+
+    def buy_medal(self):
+        self.go(False)
+        while True:
+            time.sleep(1)
+            self.driver.find_element_by_class_name("btn-plus").click()
+            time.sleep(1)
+            ele = self.driver.find_elements_by_class_name("btn-medal")[-1]
+            if ele.get_attribute("state") == "3":
+                break
+            ele.click()
+            time.sleep(1)
+            self.driver.find_element_by_class_name("btn-usual-ok").click()
+            time.sleep(1)
+            self.driver.refresh()
+
+    def run(self, start=7):
+        self.go()
+        self.move_page(1)
+        self.daily(start)
+        self.buy_medal()
